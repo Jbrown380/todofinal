e2aef33badaa1d73a93163f8870036776abe4eef
Jaden Brown <jadenmb01@gmail.com>
2021-05-02 23:14:17 -0400

    initial commit

--- todo.js
+++ todo.js
@@ -1,54 +1,34 @@
-let todo = [];
-const todoField = document.querySelector("#todoField")
-const theSection = document.querySelector("section")
+ var todos = [];
+    document.getElementById('button').addEventListener('click', function(event) {
+ event.preventDefault();
+        var list = document.getElementById('myTodo');
+ list.innerHTML = ''; 
+      var todoInput = document.getElementById('todoField').value;
+    todos.push(todoInput)  
+
+    var item = document.createElement("li");
+    for (var i = 0; i < todos.length; i++) {
+        var listItem = document.createTextNode(todos[i]);
+       var li = document.createElement("li");
+  li.appendChild(document.createTextNode(todos[i]));
+  list.append(li);
+   
+  var deleteButton = document.createElement("button");
+  deleteButton.innerText = "X";
+  deleteButton.addEventListener("click", function removeTodo() {
+    todoInput.remove();
+});
+  
+  
+  var checkBox = document.createElement("input");
+  checkBox.type = 'checkbox';
+  
+  list.appendChild(checkBox);
+  list.appendChild(deleteButton);
 
 
-document.querySelector(`form`).addEventlistener("submit", function(event) {
-    todo = todoField.value;
-    let template =
-    `
-    <ul>Todo</ul>
-    <li>${todo}</li>
-    `;
-    theSection.innerHTML = template;
 
 
-    var mylist = document.getElementsByTagName("LI");
-    var i;
-    for (i = 0; i < mylist.length; i++) {
-      var span = document.createElement("SPAN");
-      var txt = document.createTextNode("\u00D7");
-      span.className = "close";
-      span.appendChild(txt);
-      myNodelist[i].appendChild(span);
 
-
-    function newElement() {
-        var li = document.createElement("li");
-        var inputValue = document.getElementById("myInput").value;
-        var r = document.createTextNode(inputValue);
-        li.appendChild(r);
-        if (inputValue === []) {
-        
-        } else {
-          document.getElementById("form").appendChild(li);
-        }
-        document.getElementById("input").value = "";
-        var spann = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(spann);
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-        if (todo === ``)
-        todoField = ``
-        else
-        todoField = todo
-    
     }
-   }
-  }  
- }
-})   
-
+});
